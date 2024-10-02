de12495ad17e62c8dcd081b9f92c92b339e83790
Heloísa Real <heloisareal17@gmail.com>
2024-10-02 16:14:22 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // IMPORT DE COMPONENTS
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 
 const App = () => {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
   )
 }
 
